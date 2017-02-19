eeaf1c7d76b2e92e533c4da0076c39d4bd59864a
raomengnan <raomengnan@qq.com>
2017-02-19 18:56:56 +0800

    补充提交

---
+++ auto_reply/Stand.py
@@ -0,0 +1,22 @@
+# __*__coding__*__
+
+hello = ["你好", "您好", "hello", "HELLO", "Hello"]
+
+def isSayHello(str):
+    """
+    :param str:
+    :return:
+    1 short hello
+    0 lang hello
+    -1 not say hello
+
+    """
+    for h in hello:
+        if h not in str:
+            continue
+        if str.__len__() > 5:
+            return 0
+        else:
+            return 1
+    return -1
+
